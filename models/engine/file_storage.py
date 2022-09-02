8793d8ed002eee7d26e96c6b4c8be1c8a57ee709
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-09-01 20:51:53 -0700

    The Engine and Json

---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Importing the necessary"""
+import json
+
+class FileStorage:
+    """A json storage unit
+    some private class attributes initialized
+    __file_path (str): file to save objects to.
+    __objects (dict): A dictionary of instantiated objects.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id"""
+        FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        new = FileStorage.__objects
+        objectdict = {obj: new[obj].to_dict() for obj in new.keys()}
+        with open(FileStorage.__file_path, "w") as doc:
+            json.dump(objectdict, doc)
+
+        def reload(self):
+            """deserializes the JSON file to __objects (only if the JSON file exists"""
+            try:
+                with open(File.__file_path) as doc:
+                    objectdict = json.load(doc)
+                    for o in objectdict.values():
+                        cls_name = o["__class__"]
+                        del o["__class__"]
+                        self.new(eval(cls_name)(**o))
+            except FileNotFoundError:
+                return
